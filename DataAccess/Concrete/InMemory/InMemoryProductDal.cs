ae5a0a1c3b656d8b49922107073890fe6ff3f9b2
yigityesilada <yy.yesilada@gmail.com>
2021-04-04 18:32:49 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+	public class InMemoryProductDal : IProductDal
+	{
+
+		List<Product> _products;
+
+		public InMemoryProductDal()
+		{
+			_products = new List<Product> {
+
+				new Product{ProductId = 1, CategoryId = 1, ProductName = "Glass", UnitPrice = 15, UnitsInStock = 1874},
+				new Product{ProductId = 2, CategoryId = 1, ProductName = "Lamb", UnitPrice = 9, UnitsInStock = 568},
+				new Product{ProductId = 3, CategoryId = 2, ProductName = "Camera", UnitPrice = 1250, UnitsInStock = 14},
+				new Product{ProductId = 4, CategoryId = 2, ProductName = "Telephone", UnitPrice = 4000, UnitsInStock = 85},
+				new Product{ProductId = 5, CategoryId = 2, ProductName = "Keyboard", UnitPrice = 99, UnitsInStock = 128},
+				new Product{ProductId = 6, CategoryId = 2, ProductName = "Mouse", UnitPrice = 75, UnitsInStock = 1},
+			};
+		}
+
+
+		public void Add(Product product)
+		{
+			_products.Add(product);
+		}
+
+		public void Delete(Product product)
+		{
+			Product productToDelete = productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+			_products.Remove(productToDelete);
+		}
+
+		public List<Product> GetProducts()
+		{
+			return _products;
+		}
+
+		public void Update(Product product)
+		{
+			Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+			productToUpdate.CategoryId = product.CategoryId;
+			productToUpdate.ProductName = product.ProductName;
+			productToUpdate.UnitPrice = product.UnitPrice;
+			productToUpdate.UnitsInStock = product.UnitsInStock;
+		}
+
+		public List<Product> GetProductsByCategory(int categoryId)
+		{
+			return _products.Where(p => p.CategoryId == categoryId).ToList();
+		}
+	}
+}
